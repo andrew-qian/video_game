83462568bbe384deca67673fb1f1b35659d1a207
Michael <weikai.qian@gmail.com>
2018-05-16 15:47:10 -0500

    copied over files

---
+++ practice/classes.py
@@ -0,0 +1,21 @@
+import time
+
+'''
+class SampleClass:
+	def __init__(self):
+		print('i am initializing variables')
+		self.var = 'this is a variable'
+	
+	# this function returns True because i want it to
+	def print_var(self):
+		print(self.var)
+		return True
+
+
+s = SampleClass()
+return_val = s.print_var()
+
+print('This value was returned', return_val)
+'''
+
+
---
+++ practice/loops.py
@@ -0,0 +1,36 @@
+# print out the squares of each number in the list 
+# for all squares that are greater than 100
+
+numbers = [-1, 1, 2, 3, 4, 15, 13, 14]
+
+for n in numbers:
+	if n*n > 100:
+		print(n*n)
+	else:
+		None
+
+'''
+# print out the squares of each number in the list 
+# ex. [2, 3, 4] should print the following:
+#    4
+#    9
+#    16
+
+numbers = [-1, 1, 2, 3, 4, 15, 13, 14]
+
+for n in numbers:
+	print(n*n)
+'''
+
+'''
+# print out all values in the list that are greater than 10
+
+numbers = [1, 2, 3, 4, 15, 13, 14]
+
+for n in numbers:
+	if n < 10:
+		print(n)
+	else:
+		# Do nothing
+		None
+'''
